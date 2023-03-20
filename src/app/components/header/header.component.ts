cc8979c11e4d87d2ea3f20bc72b7defa5f6fad64
Diogo <diogozang2@gmail.com>
2023-03-20 20:22:52 -0300

    fix: small color and UI corrections
    small color and UI corrections

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,17 +6,4 @@ import { ThemeService } from "src/app/services/theme.service";
   styleUrls: ['./header.component.scss'],
   selector: 'app-header',
 })
-export class HeaderComponent implements OnInit {
-
-  public imageUrl = "";
-
-  constructor(
-    private themeService: ThemeService
-  ) {}
-
-  ngOnInit() {
-    const theme = this.themeService.getTheme();
-    this.imageUrl = theme === "light" ? "assets/images/diogo-light.svg" : "assets/images/diogo-dark.svg";
-  }
-
-}
+export class HeaderComponent {}
