6dcd966e70ec79e378c5523c0e9e2258fcf47f9e
Diogo <diogozang2@gmail.com>
2023-04-28 23:14:50 -0300

    trying to fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
 import { BlogComponent } from './modules/blog/blog.component';
+import { SaberInglesComponent } from './modules/blog/posts/01-saber-ingles/saber-ingles.component';
+import { WhatToChooseComponent } from './modules/blog/posts/02-what-to-choose/what-to-choose.component';
 
 const routes: Routes = [
   {
@@ -11,16 +13,16 @@ const routes: Routes = [
   },
   {
     path: 'blog',
-    component: BlogComponent,
+    loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule)
   },
   {
     path: '**',
-    component: ErrorComponent 
+    component: ErrorComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
